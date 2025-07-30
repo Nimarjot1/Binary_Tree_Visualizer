023fe46250a36671b1e2b0a17952266b037a29f8
Nimarjot kaur <157558006+Nimarjot1@users.noreply.github.com>
2025-07-30 12:13:59 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  safelist: [
+    'fab',
+    'fa-github',
+    'fa-linkedin',
+    'text-blue-400',
+    'text-gray-400',
+  ],
   theme: {
     extend: {},
   },
